f6dcf547ea4bd741c3c8b551eb3d9b1fc88acb54
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-10-07 04:44:01 +0600

    Добавлен шаблон деального просмотра задачи, редактирования

--- to_do_list/webapp/views/tasks.py
+++ to_do_list/webapp/views/tasks.py
@@ -1,8 +1,21 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 from webapp.forms import TaskForm
 from webapp.models import Tasks
 from django.views.generic import TemplateView
 
+
+
+class TaskDetailView(TemplateView):
+    template_name = 'task_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Tasks, pk=kwargs['pk'])
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
 
 
 class TaskCreateView(TemplateView):
@@ -10,6 +23,7 @@ class TaskCreateView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['tasks'] = Tasks.objects.all()
         return context
 
     def get(self, request, *args, **kwargs):
@@ -19,7 +33,44 @@ class TaskCreateView(TemplateView):
         return self.render_to_response(context)
 
     def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
         form = TaskForm(request.POST)
         if form.is_valid():
-            Tasks.objects.create(**form.cleaned_data)
+            context['tasks'].create(**form.cleaned_data)
             return redirect('index')
+
+
+class TaskUpdateView(TemplateView):
+    template_name = 'task_edit.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Tasks, pk=kwargs['pk'])
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = TaskForm(instance=context['task'])
+        context['form'] = form
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        task = get_object_or_404(Tasks, pk=kwargs['pk'])
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            return redirect('index')
+
+
+class TaskDeleteView(TemplateView):
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Tasks, pk=kwargs['pk'])
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['task'].delete()
+        return redirect('index')
